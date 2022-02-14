d459b4bcac31b0755bf7dcf58e7543b886cecf29
ThomasRStanton <thomasrstantonjr@gmail.com>
2022-02-14 17:51:34 -0600

    Fixed line stripping bug

--- ingest.py
+++ ingest.py
@@ -2,6 +2,7 @@ import sys
 import json
 
 def proc():
+    
     # First argument is the json file name
     if len(sys.argv) <= 1:
         print ("Missing json file")
@@ -12,15 +13,16 @@ def proc():
         with open(sys.argv[1], 'rb') as f:
             line = f.readline().strip();
             for i in range(5000):
-                 = f.readline()
+                _ = f.readline()
             while line:
                 obj = json.loads(line)
                 if 'tcp' in obj['_source']['layers']:
-                    #if obj['_source']['layers']['tcp']['tcp.flags_tree']['tcp.flags.syn'] == '1':
+                    if obj['_source']['layers']['tcp']['tcp.flags_tree']['tcp.flags.syn'] == '1':
+                        print(obj['_source']['layers']['ip']['ip.src'])
                     if obj['_source']['layers']['tcp']['tcp.flags'] == '0x00000018':
                         print(obj['_source']['layers']['ip']['ip.src'])
                         print(obj['_source']['layers']['ip']['ip.dst'])
-                        #line = f.readline.strip()
+                line = f.readline().strip()
     except (IOError, KeyError) as e:
         print (str(e))
         sys.exit(-1)
