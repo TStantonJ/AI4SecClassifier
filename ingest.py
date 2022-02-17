b1ca0bd329c0c461443d00a39f07c2b47e39cbe5
ThomasRStanton <thomasrstantonjr@gmail.com>
2022-02-17 13:44:09 -0600

    Created final ingest converts compressed json into single line compressed json

--- ingest.py
+++ ingest.py
@@ -1,44 +1,62 @@
 import sys
 import json
+import gzip
+from pathlib import Path
 
 def proc():
     
     # First argument is the jq file name
     if len(sys.argv) <= 1:
-        print ("Missing jq file")
-        print ('Usage: '+sys.argv[0]+" <jq file>")
+        print ("Missing json file")
+        print ('Usage: '+sys.argv[0]+" <json file>")
         sys.exit(-1)
-    if len(sys.argv) <= 2:
-        print ("Missing command")
-        print ('Usage: '+sys.argv[0]+" <syn-ack>")
-        sys.exit(-1)
-    if len(sys.argv) <= 3:
-        print ("Missing src or dst")
-        print ('Usage: '+sys.argv[0]+" <src/dst>")
-        sys.exit(-1)
-    
-    # Check for syn-acks being sent.
-    if sys.argv[2] == 'syn-ack':
-        try:
-            with open(sys.argv[1], 'rb') as f:
-                line = f.readline().strip();
-                for i in range(5000):
-                    _ = f.readline()
-                while line:
-                    obj = json.loads(line)
-                    if 'tcp' in obj['_source']['layers']:
-                        #if obj['_source']['layers']['tcp']['tcp.flags_tree']['tcp.flags.syn'] == '1':
-                        #    print(obj['_source']['layers']['ip']['ip.src'])
-                        if obj['_source']['layers']['tcp']['tcp.flags'] == '0x00000018':
-                            if sys.argv[3] == 'src':
-                                print(obj['_source']['layers']['ip']['ip.src'])
-                            else:
-                                print(obj['_source']['layers']['ip']['ip.dst'])
-                    line = f.readline().strip()
-        except (IOError, KeyError) as e:
-            print (str(e))
-            sys.exit(-1)
 
+    try:
+        counter = 0
+        # Open new file (in file + _CONVERTED) to write the new data to
+        inName = Path(sys.argv[1]).resolve().stem
+        with gzip.open(inName.rsplit('.', 1)[0]+'_CONVERTED.json.gz', 'wb') as f_out:
+            # Convert data
+            in_handle = gzip.open(sys.argv[1], 'r')
+            record = ''
+            obj = ''
+            for line in in_handle:
+                line = line.rstrip()
+                line = line.decode('utf-8')
+                counter += 1
+
+                # Strip off start and end of json list
+                if line == '[':
+                    continue
+                if line == ']':
+                    continue
+                # Strip off comma seperating json objects if at end of object
+                if line == '  },':
+                    line = '  }'
+                    line = line.lstrip()
+                    record += line
+
+                    #Add complete json object to string array with new line at end
+                    recordHolder = json.loads(record)
+                    obj += json.dumps(recordHolder)
+                    obj += '\n'
+                    #print(record)
+                    record = ''
+
+                    #Debug break on complete objects
+                    if counter > 1000:
+                        break
+                else:
+                    line = line.lstrip()
+                    record += line
+
+            #Write complete object to new file
+            obj = obj.encode('utf-8')
+            f_out.write(obj)
+
+    except (IOError, KeyError) as e:
+        print (str(e))
+        sys.exit(-1)
 
 if __name__ == '__main__':
     proc()
